f0069d7dc785056b5ef19258cb2665806b313b70
tuanngociuit98 <tuanngociuit98@gmail.com>
2022-04-12 10:30:13 +0700

    setup front-end of website

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">toogle</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
+
+export default App;
